0a8ebc79f36094fb5cd6ec97bf59f9cb178dfc8d
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-01 19:03:37 -0400

    Ejercicios 1 y 2

---
+++ app.js
@@ -0,0 +1,31 @@
+//Ejercicio 1
+// Ejercicio que invierta el orden de los caracteres
+// En consola:
+// let myText = "Hello world"
+// textReverse = ""
+// for (letter of myText)
+// {
+//     textReverse = letter + textReverse
+// }
+
+ 
+//Ejercicio 2
+// Ejercicio que dado un string, cree una función que 
+// invierta el orden de los caracteres. 
+// Debe utilizar una desetructuración en el proceso
+// En consola:
+// myText
+// 'Hello world'
+// [...myText]
+// (11) ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+// let myTextArrayChar = [...myText]
+// undefined
+// myTextArrayChar
+// (11) ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+// myTextArrayChar.reverse()
+// (11) ['d', 'l', 'r', 'o', 'w', ' ', 'o', 'l', 'l', 'e', 'H']
+
+
+
+
+
